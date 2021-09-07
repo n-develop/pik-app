ef5ac8ee1a129b9c5ad261375d51b49ec2a1f1a2
Lars Richter <github@n-develop.com>
2021-09-07 20:13:54 +0200

    Initial Commit

---
+++ People I Know/People_I_KnowApp.swift
@@ -0,0 +1,17 @@
+//
+//  People_I_KnowApp.swift
+//  People I Know
+//
+//  Created by Lars Richter on 07.09.21.
+//
+
+import SwiftUI
+
+@main
+struct People_I_KnowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
