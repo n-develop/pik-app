92169deebeb6de612101db5fd9083de2f7da4ff0
Lars Richter <github@n-develop.com>
2021-09-07 20:43:44 +0200

    Some initial work

--- People I Know/ContentView.swift
+++ People I Know/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("People I Know")
+                .font(.title)
+            List(contacts, id:\.email) { item in
+                
+                HStack(alignment:.top, spacing: 15) {
+                    Image("unknown")
+                        .resizable()
+                        .scaledToFit()
+                                            .frame(minWidth: 40, idealWidth: 50, maxWidth: 70, minHeight: 40, idealHeight: 50, maxHeight: 70, alignment: .center)
+                        
+                    VStack(alignment:.leading){
+                        Text("\(item.firstname) \(item.lastname)")
+                        Text("\(item.email)")
+                        Text("\(item.address)")
+                    }
+                }
+            }
+        }
     }
 }
 
---
+++ People I Know/Model/Contact.swift
@@ -0,0 +1,21 @@
+//
+//  Contact.swift
+//  People I Know
+//
+//  Created by Lars Richter on 07.09.21.
+//
+
+import Foundation
+
+let contacts: [Contact] = [
+    Contact(firstname: "Lars", lastname: "Richter", email: "lars@my-awesome-url.whoop", address: "Somestreet 123"),
+    Contact(firstname: "John", lastname: "Smith", email: "john@workaround-ltd.com", address: "Boulevard 123"),
+    Contact(firstname: "Jane", lastname: "Smith", email: "jane@workaround-ltd.com", address: "Boulevard 123")
+]
+
+struct Contact {
+    var firstname: String
+    var lastname: String
+    var email: String
+    var address: String
+}
