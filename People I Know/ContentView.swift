c43dd606cbea5c7ef6b4946f22b5f51e0af41741
Lars Richter <github@n-develop.com>
2021-10-11 17:38:22 +0200

    Minor styling

--- People I Know/ContentView.swift
+++ People I Know/ContentView.swift
@@ -9,25 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     
-    
     var body: some View {
         VStack{
             Text("People I Know")
                 .font(.title)
             List(contacts, id:\.email) { item in
                 
-                HStack(alignment:.top, spacing: 15) {
+                HStack(alignment:.top, spacing: 5.0) {
                     Image("unknown")
                         .resizable()
                         .scaledToFit()
                                             .frame(minWidth: 40, idealWidth: 50, maxWidth: 70, minHeight: 40, idealHeight: 50, maxHeight: 70, alignment: .center)
+                        .cornerRadius(50)
                         
                     VStack(alignment:.leading){
                         Text("\(item.firstname) \(item.lastname)")
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                         Text("\(item.email)")
+                            .font(.caption)
                         Text("\(item.address)")
+                            .font(.caption)
                     }
+                    Spacer()
                 }
+                .cornerRadius(15)
             }
         }
     }
